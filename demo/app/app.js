1685bc70285d68529204f87803d3c50555473ff6
Ashik <ashikes@gmail.com>
2014-01-28 17:35:18 +0530

    Adding grid router for reports render list view.

--- demo/app/app.js
+++ demo/app/app.js
@@ -1,16 +1,46 @@
-'use strict';
-
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'views/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'views/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('ngDynaForm', [
+        'ui.router'
+    ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'views/home.html'
+        })
+        .state('list', {
+            url: '/list',
+            templateUrl: 'views/list.html',
+            controller: 'ListCtrl'
+        })
+        .state('list.item', {
+            url: '/:item',
+            templateUrl: 'views/list.item.html',
+            controller: function($scope, $stateParams) {
+                $scope.item = $stateParams.item;
+            }
+        })
+
+   })
+
+
+.controller('ListCtrl', function ($scope) {
+      $scope.reportsList = [
+        {name: 'AgileBurnDownChart'},
+        {name: 'SprintBurnDownChart'},
+        {name: 'ReleaseBurnDownChart'},
+        {name: 'SprintBurnUpChart'},
+        {name: 'DefectsBurnDownChart'}
+    ];
+
+    $scope.selectedItem = function(selectedItem) {
+        _($scope.reportsList).each(function(item) {
+            item.selected = false;
+            if(selectedItem === item) {
+                selectedItem.selected = true;
+            }
+        });
+    };
+});
+
