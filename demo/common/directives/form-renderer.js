92db6ce6a0a0bd713d6dc0376ee9077e590580ef
Ashik <ashikes@gmail.com>
2014-02-11 15:44:54 +0530

    Added a preview option for form fields.

--- demo/common/directives/form-renderer.js
+++ demo/common/directives/form-renderer.js
@@ -35,6 +35,43 @@ angular.module('form-renderer', [])
             return templateUrl;
         };
 
+        var getOptionsData = function (objectType) {
+
+            var optionsData = '';
+            switch (objectType) {
+                case 'plan':
+                    optionsData = [
+                        {"id": "plan1001", "title": "plan1001"},
+                        {"id": "plan1002", "title": "plan1002"},
+                        {"id": "plan1003", "title": "plan1003"},
+                        {"id": "plan1004", "title": "plan1004"},
+                        {"id": "plan1005", "title": "plan1005"},
+                        {"id": "plan1006", "title": "plan1006"},
+                        {"id": "plan1007", "title": "plan1007"},
+                        {"id": "plan1008", "title": "plan1008"},
+                        {"id": "plan1009", "title": "plan1009"},
+                        {"id": "plan1010", "title": "plan1010"}
+                    ];
+                    break;
+                case 'tracker':
+                    optionsData = optionsData = [
+                        {"id": "tracker1001", "title": "tracker1001"},
+                        {"id": "tracker1002", "title": "tracker1002"},
+                        {"id": "tracker1003", "title": "tracker1003"},
+                        {"id": "tracker1004", "title": "tracker1004"},
+                        {"id": "tracker1005", "title": "tracker1005"},
+                        {"id": "tracker1006", "title": "tracker1006"},
+                        {"id": "tracker1007", "title": "tracker1007"},
+                        {"id": "tracker1008", "title": "tracker1008"},
+                        {"id": "tracker1009", "title": "tracker1009"},
+                        {"id": "tracker1010", "title": "tracker1010"}
+                    ];
+                    break;
+            }
+            return optionsData;
+        };
+
+
         var linker = function (scope, element, attrs) {
 
             // GET template content according to field type
@@ -53,6 +90,11 @@ angular.module('form-renderer', [])
                 scope.$watch("myModel", function (value) {
                     modelExpression.assign(scope.$parent, value);
                 });
+            }
+
+            //process business objects.
+            if (scope.field.object) {
+                scope.field.options = getOptionsData(scope.field.object)
             }
 
         }
